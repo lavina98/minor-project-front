1ec156c944d0412e1954a44d870ba47275ded51a
lavina <luvina1009@gmail.com>
2018-10-31 01:14:51 +0530

    user reg login connect to middle wear

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,17 +9,20 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private authService: AuthenticationService) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       email: this.fb.control('', [Validators.required, Validators.email]),
-      password: this.fb.control('', [Validators.required])
+      pwd: this.fb.control('', [Validators.required])
     });
   }
 
   onSubmit() {
     console.log(this.loginForm);
+    this.authService.loginUser(this.loginForm.value).subscribe(
+      (data) => console.log(data)
+    );
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -9,20 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private authservice: AuthenticationService) { }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
       name: this.fb.control('', [Validators.required]),
+      surname: this.fb.control('', [Validators.required]),
       email: this.fb.control('', [Validators.required, Validators.email]),
-      uclass: this.fb.control('', Validators.required),
-      phone: this.fb.control('', [Validators.required, Validators.minLength(10),
+      u_class: this.fb.control('', Validators.required),
+      mobile_number: this.fb.control('', [Validators.required, Validators.minLength(10),
                          Validators.maxLength(10)]),
-      password: this.fb.control('', [Validators.required])
+      pwd: this.fb.control('', [Validators.required])
     });
   }
 
   onSubmit() {
     console.log(this.registerForm);
+    this.authservice.registerUser(this.registerForm.value).subscribe(
+      (data) => console.log(data)
+    );
   }
 }
